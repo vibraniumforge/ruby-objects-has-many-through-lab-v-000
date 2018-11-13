26cbc420938bdca64b66ae32d9f603b5671da8ff
Greg Morgan <gregm9999@gmail.com>
2018-11-13 03:15:53 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,37 +1,12 @@
-# class Doctor
-#
-#   attr_accessor :name, :appointments
-#   @@all=[]
-#
-#   def initialize(name)
-#     @name=name
-#     @@all<<self
-#     @appointments=[]
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def new_appointment(patient, date)
-#     Appointment.new(patient, self, date)
-#   end
-#
-#   def patients
-#     self.appointments.collect {|appointment| appointment.patient}
-#   end
-#
-# end
-
 class Doctor
-  attr_accessor :name, :appointments
-  @@all = []
 
+  attr_accessor :name, :appointments
+  @@all=[]
 
   def initialize(name)
-    @name = name
-    @@all << self
-    @appointments = []
+    @name=name
+    @@all<<self
+    @appointments=[]
   end
 
   def self.all
@@ -45,4 +20,5 @@ class Doctor
   def patients
     self.appointments.collect {|appointment| appointment.patient}
   end
+
 end
