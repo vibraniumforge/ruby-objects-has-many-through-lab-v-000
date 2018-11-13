33a75011e91b0339019db13ce006d244e40e564b
Greg Morgan <gregm9999@gmail.com>
2018-11-13 01:29:39 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,6 +1,13 @@
 class Genre
-  attr_accessor :genre
-  def initialize(genre)
+  attr_accessor :name
+
+  def initialize(name)
+    @name=name
     @genre=genre
+    genre.add_song(self)
+  end
+
+  def method_name
+
   end
 end
