c3015627fc944c5bed96a395bab36cf5bb731d24
Greg Morgan <gregm9999@gmail.com>
2018-11-13 01:35:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  def initialize(name, genre)
+  def initialize(name, artist, genre)
     @name=name
     self.genre=genre
     genre.add_song(self)
